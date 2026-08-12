d83bfae79db2604e91d5a65d9ee9e30c3b7f0989
Ashley Butler <103492611@student.swin.edu.au>
2026-08-12 17:35:26 +1000

    why is it updating ?

--- NUnitTestDemo/appTest/obj/Debug/net10.0/appTest.AssemblyInfo.cs
+++ NUnitTestDemo/appTest/obj/Debug/net10.0/appTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c6664be9fc288e50bfc0588304092d773b4d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275a4358d449ef7d3267b8a35edee28be16b01ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("appTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Week1/obj/Debug/net10.0/OOP.AssemblyInfo.cs
+++ Week1/obj/Debug/net10.0/OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+275a4358d449ef7d3267b8a35edee28be16b01ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
