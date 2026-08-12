558a6b7a724f91682d79447f96846e35b20ad84d
Ashley Butler <103492611@student.swin.edu.au>
2026-08-12 19:09:37 +1000

    added git ignore file

--- Week2/wk2AssessableTask/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Week2/wk2AssessableTask/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83bfae79db2604e91d5a65d9ee9e30c3b7f0989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04bf33395801ead6d50d9c982d27a991c4755452")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
