1a4c84a00c1de8b66e71d1a0c1d16aa62106ca8a
Ashely <103429611@student.swin.edu.au>
2026-08-12 14:26:45 +1000

    week2 demo content

---
+++ NUnitTestDemo/app/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+class Program{
+    public static void Main(string[] args){
+
+        Message myMessage = new Message("This is my default message");
+        myMessage.Print();
+    }
+}
---
+++ NUnitTestDemo/appTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿namespace appTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2(){
+        Message myMessage = new Message("This is the default message");
+        myMessage.Print():
+        string currentContent = myMessage.GetMessage();
+
+        Assert.That(currentContent, Is.EqualTo("This is the default message"));
+    }
+}
